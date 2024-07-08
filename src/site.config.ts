be0d2b2456fb761a13f3632fcae4320f7de6ded8
turpeluren <thenotteam@gmail.com>
2024-07-09 00:10:10 +0200

    added twitter link

--- src/site.config.ts
+++ src/site.config.ts
@@ -25,7 +25,7 @@ export const siteConfig: SiteConfig = {
 	title: "Bar Total",
 	webmentions: {
 		// Webmention.io API endpoint. Get your own here: https://webmention.io/, and follow this blog post: https://astro-cactus.chriswilliams.dev/posts/webmentions/
-		link: "https://webmention.io/bartotal.netlify.app/webmention",
+		link: "https://webmention.io/bartotal.se/webmention",
 	},
 };
 
